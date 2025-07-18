c1efb64f49dfde25be70e3f57549cdeb4330a77a
Patou <75997617+xorblo-doitus@users.noreply.github.com>
2025-07-18 19:26:58 +0200

    Add level 1

--- src/world/world.gd
+++ src/world/world.gd
@@ -2,9 +2,22 @@ class_name World
 extends Node2D
 
 
-func _ready():
+var _started: bool = false
+
+
+func start():
+	if _started:
+		return
+	_started = true
+	
 	for start_lit in get_tree().get_nodes_in_group(&"start_lit"):
 		if start_lit is BurnableArea:
 			start_lit.lit = true
 		elif "burnable_area" in start_lit:
 			start_lit.burnable_area.lit = true
+
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action(&"start"):
+		start()
