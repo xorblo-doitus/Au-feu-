a97b6d2f8ead806fba24d6ab5ba42f7dca23c8ba
Patou <75997617+xorblo-doitus@users.noreply.github.com>
2025-07-18 19:06:52 +0200

    Make world reusable

--- src/world/world.gd
+++ src/world/world.gd
@@ -1,14 +1,10 @@
+class_name World
 extends Node2D
-
-@onready var burning_wheat: Node2D = $BurningWheat
 
 
 func _ready():
-	burning_wheat.burnable_area.lit = true
-	pass
-
-
-@warning_ignore("unused_parameter")
-func _process(delta):
-	
-	pass
+	for start_lit in get_tree().get_nodes_in_group(&"start_lit"):
+		if start_lit is BurnableArea:
+			start_lit.lit = true
+		elif "burnable_area" in start_lit:
+			start_lit.burnable_area.lit = true
